116e158b07fc7dcba71b8d404b0b41fb01c3a553
Keep it up Love <bryanlibante09@gmail.com>
2025-04-05 16:18:37 +0800

    user ui implement

---
+++ src/layout/user/sidebar/component/UserSidebar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { MdOutlineDashboard, MdLogout, MdOutlineChecklistRtl, MdOutlinePayments } from 'react-icons/md';
+
+export const UserSidebarList = () => {
+    return [
+        { name: 'Dashboard', section: 'Dashboard', icon: MdOutlineDashboard },
+        { name: 'Reservation', section: 'Reservation', icon: MdOutlineChecklistRtl },
+        { name: 'Logout', section: 'Logout', icon: MdLogout },
+    ];
+};
